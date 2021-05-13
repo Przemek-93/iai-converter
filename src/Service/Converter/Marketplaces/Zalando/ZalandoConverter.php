6a2029634045080f02a5b6e934f3eff82b33fc84
Przemek Wasilewski <emcekwadrat47@gmail.com>
2021-05-13 09:17:28 +0200

    Modification in zalando converter

--- src/Service/Converter/Marketplaces/Zalando/ZalandoConverter.php
+++ src/Service/Converter/Marketplaces/Zalando/ZalandoConverter.php
@@ -10,64 +10,6 @@ use SimpleXMLElement;
 
 class ZalandoConverter implements MarketplaceConverterInterface
 {
-    protected const HEADER = '<?xml version="1.0" encoding="utf-8"?><TBCATALOG version="1.4" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://api.trade-server.net/schema/all_in_one/tbcat_1_4_import.xsd"></TBCATALOG>';
-    protected const DEFAULT_LANG_SHORT = 'eng';
-    protected const ADDITIONAL_LANG_SHORT = 'pol';
-    protected const DEFAULT_LANG = 'x-default';
-    protected const ADDITIONAL_LANG = 'pl';
-    protected const CHANNEL_DE_ID = 'zade';
-    protected const CHANNEL_PL_ID = 'zapl';
-    protected const CURRENCY_DE = 'EUR';
-    protected const CURRENCY_DE_VALUE = 4.40;
-    protected const CURRENCY_PL = 'PLN';
-    protected const MATERIAL_ID = '140';
-    protected const FEMALE_CODES = ['F1', 'FL', 'FA', 'ML', 'FB', '8808', 'P2'];
-    protected const MALE_CODES = [
-        'CPR', 'PC', 'PA', 'S3', '8806', 'S0', 'SK', 'S18', 'SA', 'SL', 'S2', 'P0', 'S1',
-        'SR', 'E01', 'ST', 'SW', 'MS', 'SB', 'SN', 'PC', 'RM', 'N4', 'N9', 'SV'
-    ];
-    protected const UNISEX_CODES = ['STL', 'MLW'];
-    protected const AGE_GROUP = 'adult';
-    protected const SIZE_GRID = 'UE XS-XL';
-    protected const SEASON = 'NOOS';
-    protected const FAILURE_IMAGES = [
-        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_3.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/5/2/16925_1.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/3/4/16943_1.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/9/4/16949_1.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_1.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/5/2/16925_2.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/2/4/16942_2.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/3/4/16943_3.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/5/4/16945_2.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/9/4/16949_2.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_2.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/5/2/16925_3.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/2/4/16942_4.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/3/4/16943_2.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/5/4/16945_3.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/9/4/16949_5.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_4.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/4/2/16824_1.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/5/2/16825_1.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/6/2/16826_5.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/5/2/16925_4.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/2/4/16942_11.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/3/4/16943_4.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/5/4/16945_5.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/9/4/16949_4.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_5.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/3/2/16823_1.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/5/2/16925_5.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/2/4/16942_8.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/9/4/16949_3.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_6.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/2/4/16942_9.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_7.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/6/9/53896_7.jpg',
-        'https://merlitz.eu/data/gfx/pictures/large/7/9/53897_7.jpg'
-    ];
-
     protected Converter $converterHelper;
     protected zalandoHelper $zalandoHelper;
 
@@ -81,7 +23,7 @@ class ZalandoConverter implements MarketplaceConverterInterface
 
     public function convert(string $file): SimpleXMLExtended
     {
-        $simpleXMLMain = new SimpleXMLExtended(self::HEADER);
+        $simpleXMLMain = new SimpleXMLExtended(ZalandoHelper::HEADER);
         $simpleXML = $simpleXMLMain->addChild('PRODUCTDATA');
         $simpleXML->addAttribute('type', 'full');
         $file = XmlToArray::createArray($file);
@@ -93,7 +35,6 @@ class ZalandoConverter implements MarketplaceConverterInterface
 
             $item = $simpleXML->addChild('PRODUCT');
             $item->addChild('P_NR', $xmlProduct['@attributes']['id']);
-//            $this->addNameTag($item, $xmlProduct);
             $this->addNamesChild($item, $xmlProduct);
             $this->addTextTag($item, $xmlProduct);
             $this->addBrandTag($item, $xmlProduct);
@@ -120,6 +61,7 @@ class ZalandoConverter implements MarketplaceConverterInterface
         if ($offset) {
             $name = $description['name'][$offset]['@value'];
         }
+
         if (!$offset) {
             $name = $description['name']['@value'];
         }
@@ -140,6 +82,7 @@ class ZalandoConverter implements MarketplaceConverterInterface
         if ($offset) {
             $name = $description['short_desc'][$offset]['@value'];
         }
+
         if (!$offset) {
             $name = $description['short_desc']['@value'];
         }
@@ -151,12 +94,12 @@ class ZalandoConverter implements MarketplaceConverterInterface
     {
         $name = $item->addChild('P_NAME');
         $nameChildFirst = $name->addChild('VALUE', $this->produceSequence($xmlProduct, $item));
-        $nameChildFirst->addAttribute('xml:lang', self::DEFAULT_LANG, 'xml');
+        $nameChildFirst->addAttribute('xml:lang', ZalandoHelper::DEFAULT_LANG, 'xml');
     }
 
     protected function produceSequence(array $xmlProduct, SimpleXMLElement $item): string
     {
-        $defaultName = $this->getName($xmlProduct['description'], self::DEFAULT_LANG_SHORT, self::ADDITIONAL_LANG_SHORT);
+        $defaultName = $this->getName($xmlProduct['description'], ZalandoHelper::DEFAULT_LANG_SHORT, ZalandoHelper::ADDITIONAL_LANG_SHORT);
         $defaultNameArray = explode(' ', mb_strtolower($defaultName));
         $searchStrings = [
             'bag', 'folder', 'case', 'briefcase', 'wallet', 'handbag', 'backpack', 'holder', 'crossbody',
@@ -175,7 +118,7 @@ class ZalandoConverter implements MarketplaceConverterInterface
         $words = explode(' - ', $this->produceSequence($xmlProduct, $item));
         $pname = $item->addChild('P_NAME_KEYWORD');
         $value = $pname->addChild('VALUE', ucfirst($words[0]));
-        $value->addAttribute('xml:lang', self::DEFAULT_LANG,'xml');
+        $value->addAttribute('xml:lang', ZalandoHelper::DEFAULT_LANG,'xml');
 
         if (!isset($words[1])) {
             return;
@@ -187,7 +130,7 @@ class ZalandoConverter implements MarketplaceConverterInterface
 
         $pname = $item->addChild('P_NAME_PROPER');
         $value = $pname->addChild('VALUE', ucfirst($words[1]));
-        $value->addAttribute('xml:lang', self::DEFAULT_LANG,'xml');
+        $value->addAttribute('xml:lang', ZalandoHelper::DEFAULT_LANG,'xml');
     }
 
     protected function returnNameKeywordSequence(array $array, array $searchStrings): string
@@ -213,13 +156,13 @@ class ZalandoConverter implements MarketplaceConverterInterface
 
     protected function addProperName(SimpleXMLElement $item, array $xmlProduct): void
     {
-        $defaultName = $this->getName($xmlProduct['description'], self::DEFAULT_LANG_SHORT, self::ADDITIONAL_LANG_SHORT);
+        $defaultName = $this->getName($xmlProduct['description'], ZalandoHelper::DEFAULT_LANG_SHORT, ZalandoHelper::ADDITIONAL_LANG_SHORT);
         $defaultNameArray = explode(' ', $defaultName);
         $defaultWord = mb_strtoupper(end($defaultNameArray));
 
         $name = $item->addChild('P_NAME_PROPER');
         $nameChildFirst = $name->addChild('VALUE', $defaultWord);
-        $nameChildFirst->addAttribute('xml:lang', self::DEFAULT_LANG,'xml');
+        $nameChildFirst->addAttribute('xml:lang', ZalandoHelper::DEFAULT_LANG,'xml');
     }
 
     protected function addArticleDataTag(SimpleXMLElement $item, array $xmlProduct): void
@@ -231,14 +174,14 @@ class ZalandoConverter implements MarketplaceConverterInterface
 
         $activeData = $article->addChild('A_ACTIVEDATA');
         $activeDataChild = $activeData->addChild('A_ACTIVE', '1');
-        $activeDataChild->addAttribute('channel', self::CHANNEL_DE_ID);
+        $activeDataChild->addAttribute('channel', ZalandoHelper::CHANNEL_DE_ID);
 
         $activeDataChild = $activeData->addChild('A_ACTIVE', '1');
-        $activeDataChild->addAttribute('channel', self::CHANNEL_PL_ID);
+        $activeDataChild->addAttribute('channel', ZalandoHelper::CHANNEL_PL_ID);
 
         $article->addChild('A_ACTIVE', '1');
 
-        $this->addEan($article);
+        $this->addEan($article, $xmlProduct);
 
         $article->addChild('A_PROD_NR', $xmlProduct['@attributes']['id']);
 
@@ -251,10 +194,10 @@ class ZalandoConverter implements MarketplaceConverterInterface
         $this->addImagesTags($mediaData, $xmlProduct['images']['large']['image']);
     }
 
-    protected function addEan(SimpleXMLElement $element): void
+    protected function addEan(SimpleXMLElement $element, array $xmlProduct): void
     {
-        if (isset($element['sizes']['size']['@attributes']['code_producer'])) {
-            $element->addChild('A_EAN', $element['sizes']['size']['@attributes']['code_producer']);
+        if (isset($xmlProduct['sizes']['size']['@attributes']['code_producer'])) {
+            $element->addChild('A_EAN', $xmlProduct['sizes']['size']['@attributes']['code_producer']);
         }
     }
 
@@ -265,7 +208,7 @@ class ZalandoConverter implements MarketplaceConverterInterface
         $variant->addAttribute('key', 'size');
 
         $value = $variant->addChild('VALUE', 'One Size');
-        $value->addAttribute('xml:lang', self::DEFAULT_LANG,'xml');
+        $value->addAttribute('xml:lang', ZalandoHelper::DEFAULT_LANG,'xml');
 
         if (!isset($xmlProduct['group']['group_by_parameter']['product_value']['name'][1]['@value'])) {
             return;
@@ -276,14 +219,14 @@ class ZalandoConverter implements MarketplaceConverterInterface
         $variant->addAttribute('key', 'color');
 
         $value = $variant->addChild('VALUE', ucfirst($xmlProduct['group']['group_by_parameter']['product_value']['name'][1]['@value']));
-        $value->addAttribute('xml:lang', self::DEFAULT_LANG,'xml');
+        $value->addAttribute('xml:lang', ZalandoHelper::DEFAULT_LANG,'xml');
     }
 
     protected function addImagesTags(SimpleXMLElement $element, array $images): void
     {
         foreach ($images as $key => $value) {
             if (isset($value['@attributes'])) {
-                if (in_array($value['@attributes']['url'], self::FAILURE_IMAGES)) {
+                if (in_array($value['@attributes']['url'], ZalandoHelper::FAILURE_IMAGES)) {
                     continue;
                 }
                 $media = $element->addChild('A_MEDIA', $value['@attributes']['url']);
@@ -298,13 +241,13 @@ class ZalandoConverter implements MarketplaceConverterInterface
         $priceData = $item->addChild('A_PRICEDATA');
 
         $price = $priceData->addChild('A_PRICE');
-        $price->addAttribute('channel', self::CHANNEL_DE_ID);
-        $price->addAttribute('currency', self::CURRENCY_DE);
-        $price->addChild('A_VK', round($xmlProduct['price']['@attributes']['gross'] / self::CURRENCY_DE_VALUE, 2));
+        $price->addAttribute('channel', ZalandoHelper::CHANNEL_DE_ID);
+        $price->addAttribute('currency', ZalandoHelper::CURRENCY_DE);
+        $price->addChild('A_VK', round($xmlProduct['price']['@attributes']['gross'] / ZalandoHelper::CURRENCY_DE_VALUE, 2));
 
         $price = $priceData->addChild('A_PRICE');
-        $price->addAttribute('channel', self::CHANNEL_PL_ID);
-        $price->addAttribute('currency', self::CURRENCY_PL);
+        $price->addAttribute('channel', ZalandoHelper::CHANNEL_PL_ID);
+        $price->addAttribute('currency', ZalandoHelper::CURRENCY_PL);
         $price->addChild('A_VK', round($xmlProduct['price']['@attributes']['gross'], 2));
     }
 
@@ -312,9 +255,9 @@ class ZalandoConverter implements MarketplaceConverterInterface
     {
         $text = $item->addChild('P_TEXT');
         $textChildFirst = $text->addChild('VALUE',
-            $this->getDescription($xmlProduct['description'], self::DEFAULT_LANG_SHORT, self::ADDITIONAL_LANG_SHORT)
+            $this->getDescription($xmlProduct['description'], ZalandoHelper::DEFAULT_LANG_SHORT, ZalandoHelper::ADDITIONAL_LANG_SHORT)
         );
-        $textChildFirst->addAttribute('xml:lang', self::DEFAULT_LANG,'xml');
+        $textChildFirst->addAttribute('xml:lang', ZalandoHelper::DEFAULT_LANG,'xml');
     }
 
     protected function addBrandTag(SimpleXMLElement $item, array $xmlProduct): void
@@ -333,7 +276,7 @@ class ZalandoConverter implements MarketplaceConverterInterface
         foreach ($arraySequences as $word) {
             $keyword = $keywords->addChild('P_KEYWORD');
             $keywordChildFirst = $keyword->addChild('VALUE', $word);
-            $keywordChildFirst->addAttribute('xml:lang', self::DEFAULT_LANG,'xml');
+            $keywordChildFirst->addAttribute('xml:lang', ZalandoHelper::DEFAULT_LANG,'xml');
         }
     }
 
@@ -346,12 +289,12 @@ class ZalandoConverter implements MarketplaceConverterInterface
 
         $parameter = [];
         if (isset($xmlProduct['parameters'])) {
-            $parameter = $this->converterHelper->getParameterById($xmlProduct['parameters'], self::MATERIAL_ID);
+            $parameter = $this->converterHelper->getParameterById($xmlProduct['parameters'], ZalandoHelper::MATERIAL_ID);
         }
 
         $parameter = $this->mapMaterialToEng($parameter);
         $componentChildFirst = $component->addChild('VALUE', '100% ' . $parameter[0]);
-        $componentChildFirst->addAttribute('xml:lang', self::DEFAULT_LANG,'xml');
+        $componentChildFirst->addAttribute('xml:lang', ZalandoHelper::DEFAULT_LANG,'xml');
     }
 
     protected function mapMaterialToEng(array $parameter): array
@@ -394,7 +337,7 @@ class ZalandoConverter implements MarketplaceConverterInterface
         $tagsChild = $tag->addChild('VALUES');
         $tagsChild = $tagsChild->addChild('VALUE');
         $tagsChild->addAttribute('identifier', 'key');
-        $tagsChild->addAttribute('key', self::AGE_GROUP);
+        $tagsChild->addAttribute('key', ZalandoHelper::AGE_GROUP);
 
         $tag = $tagsData->addChild('P_TAG');
         $tag->addAttribute('identifier', 'key');
@@ -402,7 +345,7 @@ class ZalandoConverter implements MarketplaceConverterInterface
         $tagsChild = $tag->addChild('VALUES');
         $tagsChild = $tagsChild->addChild('VALUE');
         $tagsChild->addAttribute('identifier', 'key');
-        $tagsChild->addAttribute('key', self::SIZE_GRID);
+        $tagsChild->addAttribute('key', ZalandoHelper::SIZE_GRID);
 
         $tag = $tagsData->addChild('P_TAG');
         $tag->addAttribute('identifier', 'key');
@@ -410,24 +353,24 @@ class ZalandoConverter implements MarketplaceConverterInterface
         $tagsChild = $tag->addChild('VALUES');
         $tagsChild = $tagsChild->addChild('VALUE');
         $tagsChild->addAttribute('identifier', 'key');
-        $tagsChild->addAttribute('key', self::SEASON);
+        $tagsChild->addAttribute('key', ZalandoHelper::SEASON);
     }
 
     protected function getGenderByProductCode(string $codeOnCard): string
     {
-        foreach (self::MALE_CODES as $code) {
+        foreach (ZalandoHelper::MALE_CODES as $code) {
             if (strpos($codeOnCard, $code)) {
                 return 'male';
             }
         }
 
-        foreach (self::FEMALE_CODES as $code) {
+        foreach (ZalandoHelper::FEMALE_CODES as $code) {
             if (strpos($codeOnCard, $code)) {
                 return 'female';
             }
         }
 
-        foreach (self::UNISEX_CODES as $code) {
+        foreach (ZalandoHelper::UNISEX_CODES as $code) {
             if (strpos($codeOnCard, $code)) {
                 return 'unisex';
             }
--- src/Service/Converter/Marketplaces/Zalando/ZalandoHelper.php
+++ src/Service/Converter/Marketplaces/Zalando/ZalandoHelper.php
@@ -10,8 +10,64 @@ use SimpleXMLElement;
 
 class ZalandoHelper
 {
-    protected const DEFAULT_LANG_SHORT = 'eng';
-    protected const ADDITIONAL_LANG_SHORT = 'pol';
+    public const HEADER = '<?xml version="1.0" encoding="utf-8"?><TBCATALOG version="1.4" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://api.trade-server.net/schema/all_in_one/tbcat_1_4_import.xsd"></TBCATALOG>';
+    public const DEFAULT_LANG_SHORT = 'eng';
+    public const ADDITIONAL_LANG_SHORT = 'pol';
+    public const DEFAULT_LANG = 'x-default';
+    public const ADDITIONAL_LANG = 'pl';
+    public const CHANNEL_DE_ID = 'zade';
+    public const CHANNEL_PL_ID = 'zapl';
+    public const CURRENCY_DE = 'EUR';
+    public const CURRENCY_DE_VALUE = 4.40;
+    public const CURRENCY_PL = 'PLN';
+    public const MATERIAL_ID = '140';
+    public const FEMALE_CODES = ['F1', 'FL', 'FA', 'ML', 'FB', '8808', 'P2'];
+    public const MALE_CODES = [
+        'CPR', 'PC', 'PA', 'S3', '8806', 'S0', 'SK', 'S18', 'SA', 'SL', 'S2', 'P0', 'S1',
+        'SR', 'E01', 'ST', 'SW', 'MS', 'SB', 'SN', 'PC', 'RM', 'N4', 'N9', 'SV'
+    ];
+    public const UNISEX_CODES = ['STL', 'MLW'];
+    public const AGE_GROUP = 'adult';
+    public const SIZE_GRID = 'UE XS-XL';
+    public const SEASON = 'NOOS';
+    public const FAILURE_IMAGES = [
+        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_3.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/5/2/16925_1.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/3/4/16943_1.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/9/4/16949_1.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_1.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/5/2/16925_2.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/2/4/16942_2.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/3/4/16943_3.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/5/4/16945_2.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/9/4/16949_2.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_2.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/5/2/16925_3.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/2/4/16942_4.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/3/4/16943_2.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/5/4/16945_3.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/9/4/16949_5.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_4.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/4/2/16824_1.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/5/2/16825_1.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/6/2/16826_5.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/5/2/16925_4.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/2/4/16942_11.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/3/4/16943_4.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/5/4/16945_5.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/9/4/16949_4.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_5.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/3/2/16823_1.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/5/2/16925_5.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/2/4/16942_8.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/9/4/16949_3.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_6.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/2/4/16942_9.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/0/2/16820_7.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/6/9/53896_7.jpg',
+        'https://merlitz.eu/data/gfx/pictures/large/7/9/53897_7.jpg'
+    ];
+    public const FAILURE_ID = ['19860', '19389', '1214553957', '21412', '21427'];
 
     protected Converter $converterHelper;
 
@@ -21,18 +77,26 @@ class ZalandoHelper
 
     public function excludeFailureProducts(array &$nameArray, array $xmlProduct): bool
     {
-        if (strpos($xmlProduct['@attributes']['code_on_card'], 'BUTY') !== false ||  $xmlProduct['@attributes']['id'] === '19389') {
+        if (stripos($xmlProduct['@attributes']['code_on_card'], 'BUTY') !== false) {
             return false;
         }
 
-        if ($xmlProduct['category']['@attributes']['id'] === '1214553957' || stripos($xmlProduct['category']['@attributes']['name'], 'walizk')) {
+        if (in_array($xmlProduct['@attributes']['id'], self::FAILURE_ID, true)) {
+            return false;
+        }
+
+        if (stripos($xmlProduct['category']['@attributes']['name'], 'walizk')) {
+            return false;
+        }
+
+        if (in_array($xmlProduct['category']['@attributes']['id'], self::FAILURE_ID, true)) {
             return false;
         }
 
         $defaultName = $this->getName($xmlProduct['description'], self::DEFAULT_LANG_SHORT, self::ADDITIONAL_LANG_SHORT);
         $defaultNameArray = explode(' ', $defaultName);
         $defaultWord = mb_strtoupper($defaultNameArray[count($defaultNameArray) - 2] . ' ' . end($defaultNameArray));
-        if (in_array($defaultWord, $nameArray)) {
+        if (in_array($defaultWord, $nameArray, true)) {
             return false;
         }
 
@@ -54,6 +118,7 @@ class ZalandoHelper
         if ($offset) {
             $name = $description['name'][$offset]['@value'];
         }
+
         if (!$offset) {
             $name = $description['name']['@value'];
         }
